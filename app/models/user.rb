9ed9d65398837a6ff919729cb4ceff9962b1f81e
rails udemy course <john.s.swanson.mail@gmail.com>
2017-07-02 19:16:16 +0000

    fixed user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ActiveRecord::Base
   validates :username, presence: true, 
             uniqueness: {case_sensitive: false}, 
             length: { minimum: 3, maximum: 25 }
-  VALID_EMAIL_REGX = /\A[\+w\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
-            format: { with: VALID_EMAIL_REGX }
+            format: { with: VALID_EMAIL_REGEX }
   has_secure_password          
   
 end
