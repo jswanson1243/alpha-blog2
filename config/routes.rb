a820b1302b068c4b9e76d8562ee5dff5890749b7
rails udemy course <john.s.swanson.mail@gmail.com>
2017-07-06 19:08:09 +0000

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,12 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
